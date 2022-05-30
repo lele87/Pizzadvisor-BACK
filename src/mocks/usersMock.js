6c849c3364f12dadcc063a326fe7ba5b3e2ee34c
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-05-30 20:44:53 +0200

    Added user/register endpoint test

---
+++ src/mocks/usersMock.js
@@ -0,0 +1,14 @@
+const mockNewUser = [
+  {
+    name: "lillo",
+    username: "lillo",
+    password: "1234",
+  },
+  {
+    name: "lelo",
+    username: "lelo",
+    password: "$2a$10$e1b2brzqhDy9dzi9d4ftC.tfDy/4/lo1Dlks3VMm/ygqxpY7MD5ji",
+  },
+];
+
+module.exports = mockNewUser;

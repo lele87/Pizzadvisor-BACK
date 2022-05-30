84060492053b5b86084cb39eb77868062bca2152
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-05-30 21:08:36 +0200

    User Credentials schema

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { validate } = require("express-validation");
 const userRegister = require("../controllers/userControllers");
+const credentialsRegisterSchema = require("../schemas/userCredentials");
 
 const userRouter = express.Router();
 
-userRouter.post("/register", userRegister);
+userRouter.post("/register", validate(credentialsRegisterSchema), userRegister);
 
 module.exports = userRouter;

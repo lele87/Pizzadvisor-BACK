13800898d5dc861ddbd0b4e818d954abec1286f3
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-01 13:11:49 +0200

    Created Pizzeria schema model

---
+++ src/database/models/Pizzeria.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const PizzeriaSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  timetable: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  specialty: {
+    type: Array,
+  },
+});
+
+const Pizzeria = model("Pizzeria", PizzeriaSchema, "pizzerias");
+module.exports = Pizzeria;

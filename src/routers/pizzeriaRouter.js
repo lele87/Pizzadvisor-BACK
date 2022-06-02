d48a05bdae86606523a8144d6836a1075531d491
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-02 18:08:22 +0200

    Created middleware auth

--- src/routers/pizzeriaRouter.js
+++ src/routers/pizzeriaRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const getPizzerias = require("../controllers/pizzeriaControllers");
 
 const pizzeriaRouter = express.Router();
 
-pizzeriaRouter.get("/list");
+pizzeriaRouter.get("/list", getPizzerias);
 
 module.exports = pizzeriaRouter;

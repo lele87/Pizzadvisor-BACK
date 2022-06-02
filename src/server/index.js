b36cbca0ea5551acf7bc2515cbf5465d8c2f06ca
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-02 16:32:27 +0200

    Created getPizzerias controller

--- src/server/index.js
+++ src/server/index.js
@@ -5,16 +5,17 @@ const helmet = require("helmet");
 const userRouter = require("../routers/userRouter");
 const { notFoundError, generalError } = require("./middlewares/errors");
 const corsOptions = require("../utils/corsOptions");
+const pizzeriaRouter = require("../routers/pizzeriaRouter");
 
 const app = express();
 
 app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(helmet());
-
 app.use(express.json());
 
 app.use("/user", userRouter);
+app.use("/pizzerias", pizzeriaRouter);
 
 app.use(notFoundError);
 app.use(generalError);

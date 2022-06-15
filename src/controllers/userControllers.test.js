f34294707af181c64c7a8195a3adaad82bac29ea
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-06-15 13:55:31 +0200

    Added error test to user Controllers

--- src/controllers/userControllers.test.js
+++ src/controllers/userControllers.test.js
@@ -1,15 +1,12 @@
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../database/models/User");
-const { userRegister, userLogin } = require("./userControllers");
+const { userRegister } = require("./userControllers");
 
 const mockNewUser = {
   name: "lelo",
   username: "lelo",
   password: "1234",
 };
-
-const expectedToken = "aaaaaaaaaaaaaaaaaaaaaa";
 
 jest.mock("../database/models/User", () => ({
   findOne: jest.fn(),
@@ -63,62 +60,22 @@ describe("Given a register user function", () => {
       expect(mockNext).toHaveBeenCalledWith(expectedError);
     });
   });
-});
-
-describe("Given a loginUser function", () => {
-  const req = {
-    body: {
-      username: "lelo",
-      password: "lelo",
-    },
-  };
-
-  const res = {
-    status: jest.fn().mockReturnThis(),
-    json: jest.fn(),
-  };
-
-  describe("When invoked with a req object with the correct username and password", () => {
-    User.findOne = jest.fn().mockResolvedValue(true);
-    bcrypt.compare = jest.fn().mockResolvedValue(true);
-    jwt.sign = jest.fn().mockReturnValue(expectedToken);
-
-    test("Then it should call res status method status with 200", async () => {
-      const expectedStatus = 200;
-
-      await userLogin(req, res);
-
-      expect(res.status).toHaveBeenCalledWith(expectedStatus);
-    });
-    test("Then it should call the res json method with an object with the generated token like property", async () => {
-      await userLogin(req, res);
-
-      expect(res.json).toHaveBeenCalledWith({ token: expectedToken });
-    });
-  });
-
-  describe("When invoked with a req object with an incorrect username", () => {
+  describe("When invoked with a req object with a correct username and a wrong password", () => {
     test("Then it should call the next function", async () => {
-      const next = jest.fn();
+      const req = {
+        body: { name: "lillo", username: "lillo", password: "lillo" },
+      };
+
+      const expectedErrorMessage = "Wrong user data..";
+      const expectedError = new Error(expectedErrorMessage);
 
       User.findOne = jest.fn().mockResolvedValue(false);
 
-      await userLogin(req, res, next);
-
-      expect(next).toHaveBeenCalled();
-    });
-  });
-
-  describe("When invoked with a req object with a correct username and a wrong password", () => {
-    test("Then it should call the next function", async () => {
       const next = jest.fn();
 
-      User.findOne = jest.fn().mockResolvedValue(true);
-      bcrypt.compare = jest.fn().mockResolvedValue(false);
+      await userRegister(req, null, next);
 
-      await userLogin(req, res, next);
-
-      expect(next).toHaveBeenCalled();
+      expect(next).toHaveBeenCalledWith(expectedError);
     });
   });
 });
